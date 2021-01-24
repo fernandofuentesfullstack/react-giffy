928cf3da4087f8b7763d56688a1b1c7160a77b8c
fernandofuentesfullstack <fernandofuentesfullstack@gmail.com>
2021-01-24 20:42:56 +0100

    ADD: form in Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import {Link} from 'wouter';
+import React, { useState } from "react";
+import { Link, useLocation } from "wouter";
 
-const POPULAR_GIFS = ['Matrix', 'Betis', 'Panda', 'Morty'];
+const POPULAR_GIFS = ["Matrix", "Betis", "Panda", "Morty"];
 
-export default function Home () {
-    return (
-        <>
-            <h3 className='App-title'>Los gifs más populares</h3>
-            <ul>
-                {POPULAR_GIFS.map((popularGif) => (
-                    <li key={popularGif}>
-                        <Link to={`/search/${popularGif}`}>Gifs de {popularGif}</Link>
-                    </li>
-                ))}
-            </ul>
-        </>
-    )
+export default function Home() {
+  const [keyword, setKeyword] = useState("");
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    // navegar a otra ruta
+    console.log(keyword)
+  }
+
+  const handleChange = evt => {
+    setKeyword(evt.target.value)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} type="text" value={keyword} />
+      </form>
+
+      <h3 className="App-title">Los gifs más populares</h3>
+
+      <ul>
+        {POPULAR_GIFS.map((popularGif) => (
+          <li key={popularGif}>
+            <Link to={`/search/${popularGif}`}>Gifs de {popularGif}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
